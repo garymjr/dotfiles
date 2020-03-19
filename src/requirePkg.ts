8d1f45204a1470ff74b2637474d129b259851838
Gary Murray <garymjr@gmail.com>
2020-03-18 20:37:16 -0700

    Squashed 'vim/.vim/pack/neoclide/opt/coc-prettier/' content from commit f2c5959b0
    git-subtree-dir: vim/.vim/pack/neoclide/opt/coc-prettier
    git-subtree-split: f2c5959b0e8a5658017c9dcb57f03d86c3a7c484

---
+++ src/requirePkg.ts
@@ -0,0 +1,39 @@
+import { addToOutput } from './errorHandler'
+import resolveFrom from 'resolve-from'
+
+declare var __webpack_require__: any
+declare var __non_webpack_require__: any
+const requireFunc = typeof __webpack_require__ === "function" ? __non_webpack_require__ : require
+
+interface Options {
+  silent: boolean
+  ignoreBundled: boolean
+}
+
+/**
+ * Require package explicitly installed relative to given path.
+ * Fallback to bundled one if no package was found bottom up.
+ * @param {string} fspath file system path starting point to resolve package
+ * @param {string} pkgName package's name to require
+ * @returns module
+ */
+function requireLocalPkg(
+  fspath: string,
+  pkgName: string,
+  options: Options = { silent: true, ignoreBundled: false }
+): any {
+  let modulePath = resolveFrom.silent(fspath, pkgName)
+
+  if (modulePath !== void 0) {
+    try {
+      return requireFunc(modulePath)
+    } catch (e) {
+      if (!options.silent) {
+        addToOutput(`Failed to load require ${pkgName} from ${modulePath}.${options.ignoreBundled ? `` : ` Using bundled`}`, 'Error')
+      }
+    }
+  }
+
+  return options.ignoreBundled ? null : requireFunc(pkgName)
+}
+export { requireLocalPkg }
