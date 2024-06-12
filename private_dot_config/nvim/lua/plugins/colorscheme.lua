48e8dd9a7d19401f25f7bcfffac07c6d3ca02b43
Gary Murray <garymjr@gmail.com>
2024-06-12 12:52:00 -0600

    feat(neovim): switch back to kanagawa for now

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,6 @@ return {
 	{ "catppuccin", enabled = false },
 	{
 		"rebelot/kanagawa.nvim",
-		enabled = false,
 		dependencies = {
 			{
 				"LazyVim",
@@ -18,30 +17,6 @@ return {
 		},
 		opts = {
 			keywordStyle = { italic = false },
-		},
-	},
-	{
-		"ribru17/bamboo.nvim",
-		dependencies = {
-			{
-				"LazyVim",
-				opts = {
-					colorscheme = "bamboo",
-				},
-			},
-			{
-				"lazy.nvim",
-				opts = { install = { colorscheme = { "bamboo" } } },
-			},
-		},
-		opts = {
-			code_style = {
-				conditionals = { italic = false },
-				namespaces = { italic = false },
-			},
-			lualine = {
-				transparent = true,
-			},
 		},
 	},
 }
