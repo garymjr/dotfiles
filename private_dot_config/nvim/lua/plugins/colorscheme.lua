99ecabb31874a3fcffb9a4867dbcf3b273994bff
Gary Murray <garymjr@gmail.com>
2025-01-09 00:03:06 -0700

    chore(nvim): update plugin configurations and dependencies
    - Updated the commit hash for nvim-treesitter in lazy-lock.json to the latest version.
    - Reorganized the colorscheme configuration by enabling the rose-pine theme and adjusting its dependencies for better integration with LazyVim.

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -15,20 +15,17 @@ return {
   },
   {
     "rose-pine/neovim",
-    enabled = false,
     name = "rose-pine",
     lazy = false,
     priority = 1000,
-    dependencies = {
-      {
-        "LazyVim",
-        opts = {
-          colorscheme = "rose-pine",
-        },
-      },
-    },
     opts = {
       variant = "moon",
+    },
+  },
+  {
+    "LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
     },
   },
 }
