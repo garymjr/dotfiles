9e5d5af00237c2b0d05d10bd6510228ea4d67d0c
Gary Murray <garymjr@gmail.com>
2025-01-21 16:39:49 -0700

    feat: improve formatting config
    Enhanced the `format_on_save` configuration to respect `disable_autoformat` flags.

--- private_dot_config/nvim/lua/plugins/formatting.lua
+++ private_dot_config/nvim/lua/plugins/formatting.lua
@@ -22,12 +22,18 @@ return {
 			},
 		},
 		opts = {
-			format_on_save = {
-				timeout_ms = 3000,
-				async = false,
-				quiet = false,
-				lsp_format = "fallback",
-			},
+			format_on_save = function(bufnr)
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+
+				return {
+					timeout_ms = 3000,
+					async = false,
+					quiet = false,
+					lsp_format = "fallback",
+				}
+			end,
 			formatters = {
 				injected = { options = { ignore_errors = true } },
 			},
