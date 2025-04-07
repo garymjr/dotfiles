8690471a2cd28aa91743134dd64434c523b84370
Gary Murray <garymjr@gmail.com>
2025-04-07 16:32:42 -0600

    remove nushell

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,45 @@
+if status is-interactive
+    set fish_greeting
+
+    # Set $FZF_DEFAULT_COMMAND and $FZF_CTRL_T_COMMAND defaults
+    set -x FZF_DEFAULT_COMMAND 'fd --type file'
+    set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+
+    # Set $EDITOR
+    # set -x EDITOR nvim
+    # set -x VISUAL $EDITOR
+    # set -x GIT_EDITOR $EDITOR
+
+    # Set $PATH
+    set -x PATH /opt/homebrew/bin $PATH
+    set -x PATH ~/go/bin $PATH
+    set -x PATH /usr/local/bin $PATH
+    set -x PATH /opt/homebrew/opt/libpq/bin $PATH
+    set -x PATH /opt/homebrew/opt/mysql-client/bin $PATH
+    set -x PATH $HOME/.cargo/bin $PATH
+
+    # Set $GOPROXY and $GOSUMDB
+    set -x GOPROXY direct
+    set -x GOSUMDB off
+
+    # Define aliases
+    alias ls eza
+    alias chez chezmoi
+    alias lg lazygit
+
+    alias fanauth 'TERM=xterm-256color command fanauth'
+    alias ssh 'TERM=xterm-256color command ssh'
+
+    fzf_configure_bindings --directory=\ct --variables=\e\cv
+
+    # Load zoxide
+    source (zoxide init fish | psub)
+
+    # Source local settings if they exist
+    if test -s "$HOME/local.config.fish"
+        source $HOME/local.config.fish
+    end
+
+    zoxide init fish | source
+    starship init fish | source
+end
