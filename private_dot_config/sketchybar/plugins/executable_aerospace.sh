177b498af1bb368e95ff36315658987b3ceca9db
Gary Murray <garymjr@gmail.com>
2025-10-24 14:16:27 -0600

    update wm config

--- private_dot_config/sketchybar/plugins/executable_aerospace.sh
+++ private_dot_config/sketchybar/plugins/executable_aerospace.sh
@@ -2,14 +2,11 @@
 
 sketchybar --add event aerospace_workspace_change
 
-for sid in {1..5}; do
-  sketchybar --add item space.$sid left \
-    --subscribe space.$sid aerospace_workspace_change \
-    --set space.$sid \
-    background.drawing=off \
-    label.highlight_color="" \
-    icon="" \
-    label="$sid" \
-    click_script="aerospace workspace $sid" \
-    script="$CONFIG_DIR/scripts/aerospace.sh $sid"
-done
+sketchybar --add item aerospace_workspace left \
+  --subscribe aerospace_workspace aerospace_workspace_change \
+  --set aerospace_workspace \
+  background.drawing=off \
+  label.highlight_color="" \
+  icon="" \
+  label="" \
+  script="$CONFIG_DIR/scripts/aerospace.sh"
--- private_dot_config/sketchybar/scripts/executable_aerospace.sh
+++ private_dot_config/sketchybar/scripts/executable_aerospace.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
 
-# Function to get icon for workspace
-get_workspace_icon() {
-  case "$1" in
-  1) echo "" ;;
-  2) echo "" ;;
-  *) echo "" ;; # Default empty icon
-  esac
-}
+# Get current focused workspace
+FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
 
-ICON=$(get_workspace_icon "$1")
+ICON=""
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME label.color="0x44ffffff" icon.color="0x44ffffff" icon="$ICON"
-else
-  sketchybar --set $NAME label.color="0xffffffff" icon.color="0xffffffff" icon="$ICON"
-fi
+sketchybar --set $NAME label="$FOCUSED_WORKSPACE" icon="$ICON"
