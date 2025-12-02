6ec51324b01cd8a58d681bd5ecf41026ef046ba9
Gary Murray <garymjr@gmail.com>
2025-12-02 10:51:33 -0700

    sync colors

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,13 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-      flavour = "mocha",
-    },
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+    opts = {},
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "kanagawa",
     },
   },
 }
